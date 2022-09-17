116bc6aabd4a7a2d2a68bbaae04d0f0996a09d91
Houda-DE <houdadebza8@gmail.com>
2022-09-17 14:58:07 +0100

    adding texture images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import * as dat from 'dat.gui'
+import earthmap from '../img/earthmap1k.jpg'
+import jupitermap from '../img/jupitermap.jpg'
+import marsmap from '../img/mars_1k_color.jpg'
+import mercurymap from '../img/mercurymap.jpg'
+import neptunemap from '../img/neptunemap.jpg'
+import saturnmap from '../img/saturnmap.jpg'
+import saturnring from '../img/saturnringcolor.jpg'
+import sunmap from '../img/sunmap.jpg'
+import uranusmap from '../img/uranusmap.jpg'
+import uranusring from '../img/uranusringcolour.jpg'
+import venusmap from '../img/venusmap.jpg'
 
 const renderer = new THREE.WebGLRenderer()
 
